4204f5b1b187c21e92a295829a1503e374564168
luizscheidt <luizscheidt@onsign.tv>
2024-06-18 20:42:24 -0300

    Add styles and logo

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 
 const teams = [
   "abalo",
+  "after",
   "atura",
   "baile",
   "batiza",
@@ -12,12 +13,11 @@ const teams = [
   "broxa",
   "cartel",
   "camarote",
-  "chora",
   "delirio",
   "desfecho",
   "diretoria",
   "dose",
-  "encontro"
+  "encontro",
 ]
 
 const groupA = ref([])
@@ -48,66 +48,88 @@ function addTeam() {
 </script>
 
 <template>
-  <header style="margin-bottom: 30px;">
-    <h1>CATARINENSE LEGENDS</h1>
+  <div class="row">
+    <div class="card" style="width: 30rem; ">
+      <div class="card-header">
+          Grupo A
+        </div>
+        <ul class="list-group list-group-flush">
+          <!-- <li class="list-group-item" v-if="groupA.length === 0">Teams</li> -->
+          <li class="list-group-item" v-for="team in groupA"> {{ team }}</li>
+        </ul>
+      </div>
+
+      <div class="card" style="width: 30rem; ">
+      <div class="card-header">
+          Grupo B
+        </div>
+        <ul class="list-group list-group-flush">
+          <!-- <li class="list-group-item" v-if="groupB.length === 0">Teams</li> -->
+          <li class="list-group-item" v-for="team in groupB"> {{ team }}</li>
+        </ul>
+      </div>
+      </div>
+
+      <div class="row">
+        <button @click="addTeam" class="draw-button"> <img src="/Users/lipe/Downloads/LOGO CC LEGENDS.png" alt="logo" class="logo"></button>
+      </div>
+
+      <div class="row">
+      <div class="card" style="width: 30rem; margin-top: 20px;">
+      <div class="card-header">
+          Grupo C
+        </div>
+        <ul class="list-group list-group-flush">
+          <!-- <li class="list-group-item" v-if="groupC.length === 0">Teams</li> -->
+          <li class="list-group-item" v-for="team in groupC"> {{ team }}</li>
+        </ul>
+      </div>
+
+
+      <div class="card" style="width: 30rem; margin-top: 20px;">
+      <div class="card-header">
+          Grupo D
+        </div>
+        <ul class="list-group list-group-flush">
+          <!-- <li class="list-group-item" v-if="groupD.length === 0">Teams</li> -->
+          <li class="list-group-item" v-for="team in groupD"> {{ team }}</li>
+        </ul>
+      </div>
+    </div>
+    
     <span> {{ teams.join(", ") }}</span>
-
-    <div>
-      <button @click="addTeam" class="btn btn-lg btn-info" style="margin-top: 15px;">SORTEAR</button>
-    </div>
-  </header>
-    
-  <div class="row groups">
-    <div class="card" style="width: 30rem; margin-top: 20px;">
-      <div class="card-header">
-          Group A
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item" v-if="groupA.length === 0">Teams</li>
-          <li class="list-group-item" v-else v-for="team in groupA"> {{ team }}</li>
-        </ul>
-      </div>
-
-      <div class="card" style="width: 30rem; margin-top: 20px;">
-      <div class="card-header">
-          Group B
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item" v-if="groupB.length === 0">Teams</li>
-          <li class="list-group-item" v-else v-for="team in groupB"> {{ team }}</li>
-        </ul>
-      </div>
-
-      <div class="card" style="width: 30rem; margin-top: 20px;">
-      <div class="card-header">
-          Group C
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item" v-if="groupC.length === 0">Teams</li>
-          <li class="list-group-item" v-else v-for="team in groupC"> {{ team }}</li>
-        </ul>
-      </div>
-
-      <div class="card" style="width: 30rem; margin-top: 20px;">
-      <div class="card-header">
-          Group D
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item" v-if="groupD.length === 0">Teams</li>
-          <li class="list-group-item" v-else v-for="team in groupD"> {{ team }}</li>
-        </ul>
-      </div>
-    </div>
-    
-
 
 </template>
 
 <style scoped>
-.groups{
-  display: flex;
-  justify-content: space-around;
-  align-items: space-around;
+
+.card {
+  background-color: rgb(37, 37, 37);
+  color: black;
+  height: 260px;
+  margin: 10px;
+}
+
+.card-header{
+  background-color: #D4AF37;
+}
+
+.list-group-item {
+  color: #D4AF37;
+  background-color: rgb(50, 50, 50);
+}
+
+.logo{
+  height: 150px;
+  width: 150px;
+}
+
+.draw-button {
+  background-color: black;
+}
+
+.draw-button:hover {
+  border: unset;
 }
 
 
